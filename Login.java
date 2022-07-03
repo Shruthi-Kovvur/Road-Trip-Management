7142765a050fb05c9a2762db9aba36f5d6820f9d
Shruthi-Kovvur <92311397+Shruthi-Kovvur@users.noreply.github.com>
2022-07-03 18:09:52 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,675 @@
+/**
+ * 
+ */
+
+import Login.Sharetrip;
+
+/** 
+ * @author Administrator
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Login {
+	/** 
+	 * @author Administrator
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public class Login_details {
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String Username;
+
+		/** 
+		 * @return the Username
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getUsername() {
+			// begin-user-code
+			return Username;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theUsername the Username to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setUsername(String theUsername) {
+			// begin-user-code
+			Username = theUsername;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String Password;
+
+		/** 
+		 * @return the Password
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getPassword() {
+			// begin-user-code
+			return Password;
+			// end-user-code
+		}
+
+		/** 
+		 * @param thePassword the Password to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setPassword(String thePassword) {
+			// begin-user-code
+			Password = thePassword;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String Forgotpassword;
+
+		/** 
+		 * @return the Forgotpassword
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getForgotpassword() {
+			// begin-user-code
+			return Forgotpassword;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theForgotpassword the Forgotpassword to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setForgotpassword(String theForgotpassword) {
+			// begin-user-code
+			Forgotpassword = theForgotpassword;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private CreateTrip class1;
+
+		/** 
+		 * @return the class1
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public CreateTrip getClass1() {
+			// begin-user-code
+			return class1;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theClass1 the class1 to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setClass1(CreateTrip theClass1) {
+			// begin-user-code
+			class1 = theClass1;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Edittrip class12;
+
+		/** 
+		 * @return the class12
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Edittrip getClass12() {
+			// begin-user-code
+			return class12;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theClass12 the class12 to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setClass12(Edittrip theClass12) {
+			// begin-user-code
+			class12 = theClass12;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private CreateTrip createtrip;
+
+		/** 
+		 * @return the createtrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public CreateTrip getCreatetrip() {
+			// begin-user-code
+			return createtrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theCreatetrip the createtrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setCreatetrip(CreateTrip theCreatetrip) {
+			// begin-user-code
+			createtrip = theCreatetrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Edittrip edittrip;
+
+		/** 
+		 * @return the edittrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Edittrip getEdittrip() {
+			// begin-user-code
+			return edittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theEdittrip the edittrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setEdittrip(Edittrip theEdittrip) {
+			// begin-user-code
+			edittrip = theEdittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Enter_username() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Enter_password() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Forgot_p() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+	}
+
+	/** 
+	 * @author Administrator
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public class CreateTrip {
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Login_details login_details;
+
+		/** 
+		 * @return the login_details
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Login_details getLogin_details() {
+			// begin-user-code
+			return login_details;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theLogin_details the login_details to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setLogin_details(Login_details theLogin_details) {
+			// begin-user-code
+			login_details = theLogin_details;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String Startpoint;
+
+		/** 
+		 * @return the Startpoint
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getStartpoint() {
+			// begin-user-code
+			return Startpoint;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theStartpoint the Startpoint to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setStartpoint(String theStartpoint) {
+			// begin-user-code
+			Startpoint = theStartpoint;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String Endpoint;
+
+		/** 
+		 * @return the Endpoint
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getEndpoint() {
+			// begin-user-code
+			return Endpoint;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theEndpoint the Endpoint to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setEndpoint(String theEndpoint) {
+			// begin-user-code
+			Endpoint = theEndpoint;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Integer Depaturetime;
+
+		/** 
+		 * @return the Depaturetime
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Integer getDepaturetime() {
+			// begin-user-code
+			return Depaturetime;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theDepaturetime the Depaturetime to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setDepaturetime(Integer theDepaturetime) {
+			// begin-user-code
+			Depaturetime = theDepaturetime;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Edittrip edittrip;
+
+		/** 
+		 * @return the edittrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Edittrip getEdittrip() {
+			// begin-user-code
+			return edittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theEdittrip the edittrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setEdittrip(Edittrip theEdittrip) {
+			// begin-user-code
+			edittrip = theEdittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Edittrip edittrip2;
+
+		/** 
+		 * @return the edittrip2
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Edittrip getEdittrip2() {
+			// begin-user-code
+			return edittrip2;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theEdittrip2 the edittrip2 to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setEdittrip2(Edittrip theEdittrip2) {
+			// begin-user-code
+			edittrip2 = theEdittrip2;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void st_pt() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void end_pt() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void selectoptions() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void dept_time() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+	}
+
+	/** 
+	 * @author Administrator
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public class Edittrip extends Sharetrip {
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private Login_details login_details;
+
+		/** 
+		 * @return the login_details
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Login_details getLogin_details() {
+			// begin-user-code
+			return login_details;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theLogin_details the login_details to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setLogin_details(Login_details theLogin_details) {
+			// begin-user-code
+			login_details = theLogin_details;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String edittrip;
+
+		/** 
+		 * @return the edittrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getEdittrip() {
+			// begin-user-code
+			return edittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theEdittrip the edittrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setEdittrip(String theEdittrip) {
+			// begin-user-code
+			edittrip = theEdittrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private String deletetrip;
+
+		/** 
+		 * @return the deletetrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public String getDeletetrip() {
+			// begin-user-code
+			return deletetrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theDeletetrip the deletetrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setDeletetrip(String theDeletetrip) {
+			// begin-user-code
+			deletetrip = theDeletetrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		private CreateTrip createtrip;
+
+		/** 
+		 * @return the createtrip
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public CreateTrip getCreatetrip() {
+			// begin-user-code
+			return createtrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theCreatetrip the createtrip to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setCreatetrip(CreateTrip theCreatetrip) {
+			// begin-user-code
+			createtrip = theCreatetrip;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void edit_st_pt() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void edit_end_pt() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void _new() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void deletetrip() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+	}
+
+	/** 
+	 * @author Administrator
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public class Sharetrip {
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object download;
+
+		/** 
+		 * @return the download
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object getDownload() {
+			// begin-user-code
+			return download;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theDownload the download to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setDownload(Object theDownload) {
+			// begin-user-code
+			download = theDownload;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object Share;
+
+		/** 
+		 * @return the Share
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object getShare() {
+			// begin-user-code
+			return Share;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theShare the Share to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setShare(Object theShare) {
+			// begin-user-code
+			Share = theShare;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object save;
+
+		/** 
+		 * @return the save
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public Object getSave() {
+			// begin-user-code
+			return save;
+			// end-user-code
+		}
+
+		/** 
+		 * @param theSave the save to set
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void setSave(Object theSave) {
+			// begin-user-code
+			save = theSave;
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Download() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Share() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+
+		/** 
+		 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+		 */
+		public void Save() {
+			// begin-user-code
+			// TODO Auto-generated method stub
+
+			// end-user-code
+		}
+	}
+}
